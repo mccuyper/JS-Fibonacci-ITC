afddbf2fa2b0acf7d8a0b7bbf4de0d56e93caadd
Alexandr Kononuk <mccuyper@gmail.com>
2021-10-07 22:16:22 +0300

    init commit

--- script.js
+++ script.js
@@ -16,6 +16,8 @@
 let resultList = document.querySelector('#resultList')
 const searchButton = document.getElementById('searchButton');
 const result = document.getElementById('result');
+const resultSpinner = document.querySelector('#resultSpinner')
+
 const log = console.log
 function fetchData() {
   searchButton.addEventListener(
@@ -27,6 +29,7 @@ function fetchData() {
           const alert = document.querySelector('.alert')
       
           if (number>50 || number<=0){
+            alert.innerHTML = 'Choose number from 1 to 50'
             alert.classList.remove('d-none');
             document.querySelector(".sp-circle").style.display = 'none';
           }
@@ -49,12 +52,15 @@ function fetchData() {
                 return response.json();
             })
             .then(data => {
+              resultSpinner.style.display = 'block';
               // set timeout for spinner to desappear and show current result latest search(includes current input)
               setTimeout(function() {
                 result.innerHTML = `${data.result}`; 
                 document.querySelector(".sp-circle").style.display = 'none';
                 result.style.color = 'black';
                 // call func() that shows 3 latest search
+                
+                resultList.innerHTML = ''
                 loadResult()
               }, 1000)  
             })
@@ -63,22 +69,27 @@ function fetchData() {
   )}
 fetchData()
 
+window.onload = setTimeout(loadResult,2000)
+
+
 
 function loadResult() {
   const url2 = 'http://localhost:5050/getFibonacciResults'
+  
+  resultSpinner.style.display = 'none';
   fetch(url2).then(r => {
     return r.json();
   }).then(data => {
     let newObj = Object.assign({}, data);
     let newArr =   newObj.results
     newArr.sort(sortByDate)
-    log(newArr.sort(sortByDate))
     for (let i=0; i<3; i++) {   
       let jDate = new Date(newArr[i].createdDate)  
       const resultElement = `<p class="lastSearch">
       The Fibonacci of <b>${newArr[i].number}</b> is <b>${newArr[i].result}</b> Calculated at: ${jDate}
     </p>`
       resultList.insertAdjacentHTML("beforeend", resultElement)
+      
     }
   })
 }
